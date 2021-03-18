8bd2fd0d4e3af1cc170ac09400a960b6010160c3
amador-mmr <72734540+amador-mmr@users.noreply.github.com>
2021-03-18 23:51:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Layout from "./components/layouts/Layout";
+import Home from "./components/pages/Home";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Layout>
+    );
+  }
+}
+export default App;
---
+++ src/components/layouts/Layout.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Layout extends Component {
+  render() {
+    return <Container>{this.props.children}</Container>;
+  }
+}
+export default Layout;
